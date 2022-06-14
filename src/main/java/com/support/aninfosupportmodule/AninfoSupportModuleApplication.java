70c1906cf79b5a63bff29de87ba8d5cbd1fa2534
Josefa <jiterman@fi.uba.ar>
2022-06-13 23:57:38 -0300

    Revert changes in AninfoSupportModuleApplication

--- src/main/java/com/support/aninfosupportmodule/AninfoSupportModuleApplication.java
+++ src/main/java/com/support/aninfosupportmodule/AninfoSupportModuleApplication.java
@@ -2,6 +2,7 @@ package com.support.aninfosupportmodule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class AninfoSupportModuleApplication {
 
