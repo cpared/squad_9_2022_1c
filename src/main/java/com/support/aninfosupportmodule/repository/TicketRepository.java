cbc99164c0eb23ce50df1ab577ee5730a75478e5
Josefa <jiterman@fi.uba.ar>
2022-06-17 19:29:17 -0300

    add-database

---
+++ src/main/java/com/support/aninfosupportmodule/repository/TicketRepository.java
@@ -0,0 +1,9 @@
+package com.support.aninfosupportmodule.repository;
+
+import com.support.aninfosupportmodule.dto.Ticket;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path = "tickets", collectionResourceRel = "tickets")
+public interface TicketRepository extends CrudRepository<Ticket, Integer> {
+}
