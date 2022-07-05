a285d639aa5f1cf6021f8b70380d2660e99dbf54
Josefina <54693795+jiterman@users.noreply.github.com>
2022-07-04 21:28:44 -0300

    Fix get projects (#81)

--- src/main/java/com/support/aninfosupportmodule/controller/ProjectController.java
+++ src/main/java/com/support/aninfosupportmodule/controller/ProjectController.java
@@ -1,7 +1,7 @@
 package com.support.aninfosupportmodule.controller;
 
-import com.support.aninfosupportmodule.dto.Project;
-import com.support.aninfosupportmodule.dto.TaskResponse;
+import com.support.aninfosupportmodule.dto.ProjectData;
+import com.support.aninfosupportmodule.dto.TaskData;
 import com.support.aninfosupportmodule.dto.request.TaskRequest;
 import com.support.aninfosupportmodule.exception.InternalServerException;
 import com.support.aninfosupportmodule.rest.ProjectService;
@@ -19,12 +19,12 @@ public class ProjectController {
     private final ProjectService projectService;
 
     @GetMapping()
-    public List<Project> getProjects(){
+    public List<ProjectData> getProjects(){
         return projectService.getProjects();
     }
 
     @PostMapping("/task")
-    public TaskResponse createTask(@RequestBody TaskRequest request) throws InternalServerException {
+    public TaskData createTask(@RequestBody TaskRequest request) throws InternalServerException {
         return projectService.createTask(request);
     }
 }
